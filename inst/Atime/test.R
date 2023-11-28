37efa3e4a8be39106dbbb3a69cf8905e31c56084
DorisAmoakohene <143125519+DorisAmoakohene@users.noreply.github.com>
2023-11-28 09:43:54 -0700

    workflow test

---
+++ inst/Atime/test.R
@@ -0,0 +1,59 @@
+
+library(atime)
+library(data.table)
+library(ggplot2)
+library(git2r)
+
+
+tdir <- tempfile()
+dir.create(tdir)
+git2r::clone("https://github.com/Rdatatable/data.table", tdir)
+
+atime.list <- atime::atime_versions(
+  pkg.path=tdir,
+  pkg.edit.fun=function(old.Package, new.Package, sha, new.pkg.path){
+    pkg_find_replace <- function(glob, FIND, REPLACE){
+      atime::glob_find_replace(file.path(new.pkg.path, glob), FIND, REPLACE)
+    }
+    Package_regex <- gsub(".", "_?", old.Package, fixed=TRUE)
+    Package_ <- gsub(".", "_", old.Package, fixed=TRUE)
+    new.Package_ <- paste0(Package_, "_", sha)
+    pkg_find_replace(
+      "DESCRIPTION",
+      paste0("Package:\\s+", old.Package),
+      paste("Package:", new.Package))
+    pkg_find_replace(
+      file.path("src","Makevars.*in"),
+      Package_regex,
+      new.Package_)
+    pkg_find_replace(
+      file.path("R", "onLoad.R"),
+      Package_regex,
+      new.Package_)
+    pkg_find_replace(
+      file.path("R", "onLoad.R"),
+      sprintf('packageVersion\\("%s"\\)', old.Package),
+      sprintf('packageVersion\\("%s"\\)', new.Package))
+    pkg_find_replace(
+      file.path("src", "init.c"),
+      paste0("R_init_", Package_regex),
+      paste0("R_init_", gsub("[.]", "_", new.Package_)))
+    pkg_find_replace(
+      "NAMESPACE",
+      sprintf('useDynLib\\("?%s"?', Package_regex),
+      paste0('useDynLib(', new.Package_))
+  },
+  N=10^seq(3,8),
+  setup={
+    n <- N/100
+    set.seed(1L)
+    dt <- data.table(
+      g = sample(seq_len(n), N, TRUE),
+      x = runif(N),
+      key = "g")
+    dt_mod <- copy(dt)
+  },
+  expr=data.table:::`[.data.table`(dt_mod, , N := .N, by = g),
+  "Before"="be2f72e6f5c90622fe72e1c315ca05769a9dc854",
+  "Regression"="e793f53466d99f86e70fc2611b708ae8c601a451",
+"Fixed"="58409197426ced4714af842650b0cc3b9e2cb842")
